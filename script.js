f1fdbe9f84474f47e689effaafc7a1e6b3bda6d5
jsteenb2 <jonathansteenbergen@gmail.com>
2016-09-09 17:01:17 -0700

    added psuedocode

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
 GRID_SIZE = 10;
-// Snake
-// - length
+// SnakeModel
+// - body coords (array)
+// - direction
+// - getter for Snake Length [array]
+// - change current direction (depends on key, starts in top left go right default)
+// - change position of snake body (pop off end, shift to front, change coordinates)
 
-// Model
-// - Snake Length
-// - current direction
+// Food Model
+// - food drop method (can't be on snake)
+// - food coordinates
 
-
+// BoardGameModel
+// - grid size x?
+// - grid size y?
+// - set new food
+// - update snake location (calls to controller)  ** Game loop
+//  
 
 // View
 // -render
 // Event on food to see if snake has hit it
+// keypress event
 
 // Controller
-// handler for snake food
+// handler for snake food w/ conditions
+// keypress handler
 
 
 var view = {
@@ -25,7 +36,10 @@ var view = {
       var $row = $("<div class='row'></div>");
 
       for(var i = 0; i < GRID_SIZE; i++ ) {
-        $row.append("<div class='box'></div>");
+        $box = $("<div class='box'></div>")
+                .attr("data-x", i)
+                .attr("data-y", j);
+        $row.append($box);
       }
 
       $board.append($row);
